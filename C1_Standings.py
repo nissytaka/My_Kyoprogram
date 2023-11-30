bc0ec38556054661eb410423b570ab4ab955f2c6
takaaki <takaaki0210123@gmail.com>
2023-11-30 11:55:53 +0900

    試し打ち

---
+++ C1_Standings.py
@@ -0,0 +1,21 @@
+'''
+CodeQUEEN 2023 予選 (AtCoder Beginner Contest 308)
+URL: https://atcoder.jp/contests/abc308/tasks/abc308_c
+C問題
+'''
+from decimal import Decimal
+#入力
+N = int(input())
+A_B = [list(map(int,input().split())) for _ in range(N)]
+#出力
+#確率用のリストrate
+rate =[]
+ans ={0}
+#すべての確率を出している
+for i in range(N):
+    rate.append(Decimal(A_B[i][0])/Decimal((A_B[i][0])+(A_B[i][1])))
+for i in range(N):
+    ans.add(rate.index(max(rate))+1)
+    rate[rate.index(max(rate))] = 0
+ans.remove(0)
+print(*ans)
