9895c39db5f8c27f94cb695548d2137081be31b9
takaaki <takaaki0210123@gmail.com>
2023-12-02 19:47:58 +0900

    complete

---
+++ A_Thefirstproblem.py
@@ -0,0 +1,17 @@
+'''
+CodeQUEEN 2023 予選 (AtCoder Beginner Contest 308)
+URL:https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_ca
+C問題
+'''
+#入力
+A,B = map(int, input().split())
+#出力
+C = []
+a = 0
+for i in range(A,B+1):
+    if 100%i == 0:
+        a += 1
+        print('Yes')
+        break
+if a == 0:
+    print('No')
