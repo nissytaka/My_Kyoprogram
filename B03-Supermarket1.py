4428031c6da10535caaf8b6540094b6cfd1394f0
takaaki <takaaki0210123@gmail.com>
2023-12-03 15:49:09 +0900

    complete

---
+++ B03-Supermarket1.py
@@ -0,0 +1,35 @@
+'''
+問題集
+URL:https://atcoder.jp/contests/tessoku-book/tasks
+URL: https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_cb
+B問題
+'''
+#入力
+N = int(input())
+A = list(map(int, input().split()))
+#出力
+import itertools
+total = sum(A)
+a = 0
+for n in itertools.combinations(A,N-3):
+    two_no_total = sum(n)
+    if (total-two_no_total) ==1000:
+        print('Yes')
+        a += 1
+        break
+    else : 
+        two_no_total = 0
+if a == 0:
+    print('No')
+
+        
+
+
+
+
+     
+    
+    
+    
+    
+        
---
+++ B4-BinaryRepresentation.py
@@ -0,0 +1,9 @@
+'''
+問題集
+URL:https://atcoder.jp/contests/tessoku-book/tasks
+URL: https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_cc
+B問題
+'''
+#入力
+
+#出力
