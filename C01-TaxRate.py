7767a7029d3fd25c586f5f240639e04ac7b1281c
takaaki <takaaki0210123@gmail.com>
2023-12-08 16:44:36 +0900

    追加

--- C01-TaxRate.py
+++ C01-TaxRate.py
@@ -1,5 +1,11 @@
 """
 Ｓｋｙ株式会社プログラミングコンテスト2023（AtCoder Beginner Contest 289）
-URL: https://atcoder.jp/contests/abc289/tasks/abc289_a
+URL: 
 """
+#入力
+N = int(input())
+#出力
+import math
+ans = N*1.1
+print(math.floor(ans))
 
--- C01-TaxRate2.py
+++ C01-TaxRate2.py
@@ -2,4 +2,14 @@
 Ｓｋｙ株式会社プログラミングコンテスト2023（AtCoder Beginner Contest 289）
 URL: https://atcoder.jp/contests/abc289/tasks/abc289_a
 """
+#入力
+N = int(input())
+#出力
+#if N % 100 == 0:
+STR = list(map(int,str(N)))
+result = N*1.1
+ans = list(map(str,str(result)))
+print(''.join(ans[:(len(STR))]))
+
+#1問WA
 
