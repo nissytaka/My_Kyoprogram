f24043d3c9c78f94dba2aeb2da0c58b29b45626b
takaaki <takaaki0210123@gmail.com>
2023-12-08 16:36:29 +0900

    追加

--- C01-TaxRate.py
+++ C01-TaxRate.py
@@ -3,7 +3,3 @@
 URL: https://atcoder.jp/contests/abc289/tasks/abc289_a
 """
 
-N = int(input())
-import math
-ans = N*1.1
-print(math.floor(ans))
--- C01-TaxRate2.py
+++ C01-TaxRate2.py
@@ -3,13 +3,3 @@
 URL: https://atcoder.jp/contests/abc289/tasks/abc289_a
 """
 
-N = int(input())
-#if N % 100 == 0:
-STR = list(map(int,str(N)))
-result = N*1.1
-ans = list(map(str,str(result)))
-#if N >= 91000 and N<100000:
-#    print(''.join(ans[:(len(STR))+1]))
-print(''.join(ans[:(len(STR))]))
-
-#1問WA
