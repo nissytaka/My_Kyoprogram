b0af078f00648cc604b74bf695a906d27fcdcd07
takaaki <takaaki0210123@gmail.com>
2023-12-08 16:48:42 +0900

    追加

--- C_coupon.py
+++ C_coupon.py
@@ -4,22 +4,6 @@ URL: https://atcoder.jp/contests/abc246/tasks/abc246_c
 C問題
 '''
 #入力
-item,coupon,discount = map(int, input().split())
-price =list(map(int, input().split()))
-print(item)
-print(coupon)
-print(discount)
-print(price)
-#出力
-total = 0
-sa = []
-for i in range(item):
-    sa.append(price[i]-discount)
-    for n in range(coupon):
-        if not coupon == 0:
-            if   :
-            total += price[i]-X*min(sa)
-            coupon -= min(sa)
-        
+
         
     
