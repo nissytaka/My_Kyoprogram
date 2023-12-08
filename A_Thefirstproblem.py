5f3d5c426b4f7d366804c527a5dcfc1977dfd8a4
takaaki <takaaki0210123@gmail.com>
2023-12-08 16:57:12 +0900

    追加

--- A_Thefirstproblem.py
+++ A_Thefirstproblem.py
@@ -11,14 +11,4 @@ URL:https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_ca
 C問題
 '''
 #入力
-A,B = map(int, input().split())
-#出力
-C = []
-a = 0
-for i in range(A,B+1):
-    if 100%i == 0:
-        a += 1
-        print('Yes')
-        break
-if a == 0:
-    print('No')
+
