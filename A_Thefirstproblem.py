1697ad2b7fd68749f6ae281aa2c30b1f96c25800
Takaaki <takaaki0210123@gmail.com>
2025-02-25 17:26:42 +0900

    test

--- A_Thefirstproblem.py
+++ A_Thefirstproblem.py
@@ -4,7 +4,7 @@ URL:https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_ca
 C問題
 '''
 #入力
-
+#test
 '''
 CodeQUEEN 2023 予選 (AtCoder Beginner Contest 308)
 URL:https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_ca
