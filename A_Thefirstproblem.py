704c128f21069123edeecacfb0e4efd81a63b6d2
Takaaki <takaaki0210123@gmail.com>
2025-02-25 18:19:35 +0900

    修正

--- A_Thefirstproblem.py
+++ A_Thefirstproblem.py
@@ -1,18 +1,12 @@
-'''
-問題集
-URL:https://atcoder.jp/contests/tessoku-book/tasks
-CodeQUEEN 2023 予選 (AtCoder Beginner Contest 308)
-
+'''''
 A問題
 URL:https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_a
 '''
 
 #入力
-N= int(input())
 
 #出力
-answer= N**2
-print(answer)
+
 
 
 
