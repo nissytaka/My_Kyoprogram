ab658d099fff05301d7baf9a399588d60c6db4cf
Takaaki <takaaki0210123@gmail.com>
2025-02-25 17:34:26 +0900

    修正

--- A_Thefirstproblem.py
+++ A_Thefirstproblem.py
@@ -1,14 +1,14 @@
 '''
+問題集
+URL:https://atcoder.jp/contests/tessoku-book/tasks
 CodeQUEEN 2023 予選 (AtCoder Beginner Contest 308)
-URL:https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_ca
-C問題
+
+A問題
+URL:https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_a
 '''
+
 #入力
-#test
-'''
-CodeQUEEN 2023 予選 (AtCoder Beginner Contest 308)
-URL:https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_ca
-C問題
-'''
-#入力
+
+#出力
+
 
--- B02-Divisor_Check.py
+++ B02-Divisor_Check.py
@@ -1,15 +1,9 @@
 '''
-CodeQUEEN 2023 予選 (AtCoder Beginner Contest 308)
-URL:https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_ca
-C問題
-'''
-#入力
-
-'''
 問題集
 URL:https://atcoder.jp/contests/tessoku-book/tasks
-URL:https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_ca
+
 B問題
+URL:https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_ca
 '''
 #入力
 
