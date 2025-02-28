bdbcf9d930eef4e09e64457fd8fdd14bc39c5a11
Takaaki <takaaki0210123@gmail.com>
2025-02-28 15:16:18 +0900

    解答

--- B03_Supermarket1.py
+++ B03_Supermarket1.py
@@ -4,4 +4,16 @@ URL: https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_cb
 B問題
 '''
 #入力
+N=int(input())
+list = list(map(int,input().split()))
 #出力
+import itertools
+a=0
+for i in (itertools.combinations(list,3)):
+    answer=sum(i)
+    if answer ==1000:
+        a+=1
+if a>0:
+    print("Yes")    
+else:  
+    print("No")    
