ce60e80dd5d91e0d1733d14da843bbc46e40b414
Takaaki <takaaki0210123@gmail.com>
2025-02-28 14:52:09 +0900

    takachanの解答

--- A_Thefirstproblem.py
+++ A_Thefirstproblem.py
@@ -4,8 +4,10 @@ URL:https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_a
 '''
 
 #入力
+object = int(input())
 #出力
-
+answer=object**2
+print(answer)
 
 
 
--- B02-Divisor_Check.py
+++ B02-Divisor_Check.py
@@ -1,10 +1,15 @@
 '''
-問題集
-URL:https://atcoder.jp/contests/tessoku-book/tasks
-
 B問題
 URL:https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_ca
 '''
 #入力
-
+a,b = map(int,input().split())
 #出力
+N=0
+for i in range(a,b+1):
+    if 100%i ==0:
+        N +=1
+if N>0:
+    print("Yes")
+else:
+    print("No")
